68d2ecb56150daf6297701b1c2a4d9aec2ec2615
Matheus Cardoso <matheusjosecardoso@outlook.com>
2022-06-13 19:57:50 -0300

    first commit

---
+++ venda_django_naes/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for venda_django_naes project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'venda_django_naes.settings')
+
+application = get_wsgi_application()

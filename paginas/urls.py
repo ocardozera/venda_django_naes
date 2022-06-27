18c540b973c30811c4b5c096c4e37b1b97e867d7
Matheus Cardoso <matheusjosecardoso@outlook.com>
2022-06-27 20:27:34 -0300

    Aula 03

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+
+# Importa as views que a gente criou
 from .views import IndexView
 
 urlpatterns = [
+    #Todo path tem endereço, sua_view.as_view() e nome
     # path('endereço/', MinhaView.as_view(), name='nome-da-url'),
     
     path('inicio/', IndexView.as_view(), name='inicio'),
